e2df753e83f22380b2367b8e220160395ad11a7a
akhilsinghal1234 <akhilsinghal1234@gmail.com>
2017-09-13 01:46:35 +0530

    case1:done

--- pr1.py
+++ pr1.py
@@ -2,12 +2,29 @@ import sys
 import math,time
 import matplotlib.pyplot as plt
 
+def calc_slope(mean_y2,mean_y1,mean_x1,mean_x2):
+	m = (mean_y2 - mean_y1)/(mean_x2 - mean_x1)
+	return -1*(1/m)
+
+def cal_mean(array):
+	mean = 0
+	for x in range((3*len(array))//4):					#calculating mean
+		mean += array[x];
+	return mean/((3*len(array_x1))//4)
+
+def cal_var(array,mean):
+	var = 0
+	for x in range((3*len(array))//4):					#calculating variance
+		var += math.pow((array[x]-mean),2)
+	return (var/((3*len(array_x1))//4))
+
 file1 = input("Enter file name(1): \n")
 fp1 = open(file1,"r")
 file2 = input("Enter file name(2): \n")
 fp2 = open(file2,"r")
 
 array_x1,array_y1,array_x2,array_y2 = [],[],[],[]
+
 
 for line in fp1:
 	m,n = line.split()
@@ -21,67 +38,63 @@ for line in fp2:
 plt.title('Data from')
 plt.xlabel('-----------------------X-----------------------')
 plt.ylabel('-----------------------Y-----------------------')
-plt.xlim(-20,20)
-plt.ylim(-20,20)
+
 plt.scatter(array_x1,array_y1,c = 'r')
 plt.scatter(array_x2,array_y2,c = 'b')
 
-
 mean_x1,mean_y1,mean_x2,mean_y2 = 0,0,0,0
 
+mean_x1 = cal_mean(array_x1)
+mean_y1 = cal_mean(array_y1)
+mean_x2 = cal_mean(array_x2)
+mean_y2 = cal_mean(array_y2)
 
-for x in range((3*len(array_x1))//4):					#calculating mean for x & y
-	mean_x1 += array_x1[x];
-for y in range((3*len(array_y1))//4):
-	mean_y1 += array_y1[y]
-for x in range((3*len(array_x2))//4):
-	mean_x2 += array_x2[x]
-for y in range((3*len(array_y2))//4):
-	mean_y2 += array_y2[y]
-
-mean_x1 = float(mean_x1/((3*len(array_x1))//4))
-mean_y1 = float(mean_y1/((3*len(array_y1))//4))
-mean_x2 = float(mean_x2/((3*len(array_x2))//4))
-mean_y2 = float(mean_y2/((3*len(array_y2))//4))
-print("Mean x for 1: ",round(mean_x1,3))
-print("Mean y for 1:",round(mean_y1,3))
-print("Mean x for 2: ",round(mean_x2,3))
-print("Mean y for 2:",round(mean_y2,3),"\n")
+# print("Mean x for 1: ",round(mean_x1,3))
+# print("Mean y for 1:",round(mean_y1,3))
+# print("Mean x for 2: ",round(mean_x2,3))
+# print("Mean y for 2:",round(mean_y2,3))
 
 var_x1,var_x2,var_y1,var_y2 = 0, 0 , 0 , 0
 
-for x in range((3*len(array_x1))//4):					#calculating variance for x and y
-	var_x1 += math.pow((array_x1[x]-mean_x1),2)
-for y in range((3*len(array_y1))//4):
-	var_y1 += math.pow((array_y1[x]-mean_y1),2)
-for x in range((3*len(array_x2))//4):
-	var_x2 += math.pow((array_x2[x]-mean_x2),2)
-for y in range((3*len(array_y2))//4):
-	var_y2 += math.pow((array_y2[x]-mean_y2),2)
+var_x1 = cal_var(array_x1,mean_x1)
+var_y1 = cal_var(array_y1,mean_y1)
+var_x2 = cal_var(array_x2,mean_x2)
+var_y2 = cal_var(array_y2,mean_y2)
 
-co_1,co_2 = 0,0
+slope = calc_slope(mean_y2,mean_y1,mean_x1,mean_x2)
 
-for i in range((3*len(array_x1))//4):					#calculating co-var for files 1 & 2
-	co_1 += ((array_x1[i]-mean_x1)*(array_y1[i] - mean_y1))
+x0_1 = (mean_x1 + mean_x2)/2
+x0_2 = (mean_y1 + mean_y2)/2
+c = x0_2 - slope*x0_1
 
-co_1 = co_1/((3*len(array_x1)//4 ) - 1)
+plot1,plot2 = [],[]
+for x in range (-15,15):
+	plot1.append(x)
+	plot2.append(slope*x + c)
 
-for i in range((3*len(array_x2))//4):
-	co_2 += ((array_x2[i]-mean_x2)*(array_y2[i] - mean_y2))
+plt.plot(plot1, plot2, c = "g")
 
-co_2 = co_2/((3*len(array_x2)//4 ) - 1)
+# for i in range((3*len(array_x1))//4):					#calculating co-var for files 1 & 2
+# 	co_1 += ((array_x1[i]-mean_x1)*(array_y1[i] - mean_y1))
 
-var_x1 = float(var_x1/((3*len(array_x1))//4))
-var_y1 = float(var_y1/((3*len(array_y1))//4))
-var_x2 = float(var_x2/((3*len(array_x2))//4))
-var_y2 = float(var_y2/((3*len(array_y2))//4))
+# co_1 = co_1/((3*len(array_x1)//4 ) - 1)
+
+# for i in range((3*len(array_x2))//4):
+# 	co_2 += ((array_x2[i]-mean_x2)*(array_y2[i] - mean_y2))
+
+#co_2 = co_2/((3*len(array_x2)//4 ) - 1)
+
+
+# var_1 = (var_x1 + var_y1)/2
+# var_2 = (var_x2 + var_y2)/2
+# var_avg = (var_1 + var_2)/2
+# print( "Average var: ",round(var_avg,3))
 
 print("Variance x1: ",round(var_x1,2))
 print("Variance y1: ",round(var_y1,2))
 print("Variance x2: ",round(var_x2,2))
 print("Variance y2: ",round(var_y2,2))
-print("Co-Var(1): ",round(co_1,2))
-print("Co-Var(2): ",round(co_2,2))
+
 plt.show()
 fp1.close()
 fp2.close()
